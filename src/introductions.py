e24ec9a238552f52ed32b985d05a60f0d0eeeee2
Wojciech Matusiak <saphir2357@gmail.com>
2021-02-27 20:44:35 +0100

    Probably almost ready bot is here

---
+++ src/introductions.py
@@ -0,0 +1,74 @@
+import csv
+import difflib
+from typing import Optional
+
+import discord
+
+FIELDS_NAMES = ["FirstName", "LastName", "Group", "DiscordId"]
+FIRST_NAME_MATCH_RATIO_THRESHOLD = 0.50
+LAST_NAME_MATCH_RATIO_THRESHOLD = 0.90
+
+
+class NickInUseError(ValueError):
+    pass
+
+
+class Journal:
+    def __init__(self):
+        self._indexed_names = {}
+        self._indexed_ids = {}
+        self._data = []
+
+    def read(self, csv_path: str) -> None:
+        with open(csv_path) as stream:
+            reader = csv.DictReader(stream)
+            for member in reader:
+                name = (member["LastName"], member["FirstName"])
+                if name in self._indexed_names:
+                    raise ValueError(f"Name {name} appears twice in journal file")
+                if member["DiscordId"] and member["DiscordId"] in self._indexed_ids:
+                    raise ValueError(f"Nick {member['DiscordId']} appears twice in journal file")
+                self._indexed_names[name] = len(self._data)
+                if member["DiscordId"]:
+                    self._indexed_ids = len(self._data)
+                self._data.append(member)
+
+    def save(self, csv_path: str) -> None:
+        with open(csv_path, "w") as stream:
+            writer = csv.DictWriter(stream, FIELDS_NAMES, extrasaction="ignore")
+            writer.writeheader()
+            for member in self._data:
+                writer.writerow(member)
+
+    def match_name(self, first_name: str, last_name: str, discord_id: int) -> Optional[str]:
+        name = (last_name, first_name)
+        if name not in self._indexed_names:
+            return None
+        if discord_id in self._indexed_ids:
+            raise NickInUseError(f"Member with id {discord_id} requested to authorize twice")
+        idx = self._indexed_names[name]
+        self._indexed_ids[discord_id] = idx
+        member = self._data = self._data[idx]
+        assert member["FirstName"] == first_name
+        assert member["LastName"] == last_name
+        assert not member["DiscordId"]
+        member["DiscordId"] = discord_id
+        return member["Group"]
+
+    def match_name_weak(self, first_name: str, last_name: str, discord_id: int) -> Optional[str]:
+        if result := self.match_name(first_name, last_name, discord_id):
+            return result
+        first_name_matcher = difflib.SequenceMatcher(autojunk=False)
+        first_name_matcher.set_seq2(first_name)
+        last_name_matcher = difflib.SequenceMatcher(autojunk=False)
+        last_name_matcher.set_seq2(last_name)
+        for member in self._data:
+            last_name_matcher.set_seq1(member["LastName"])
+            if last_name_matcher.ratio() < LAST_NAME_MATCH_RATIO_THRESHOLD:
+                continue
+            first_name_matcher.set_seq1(member["FirstName"])
+            if first_name_matcher.ratio() >= FIRST_NAME_MATCH_RATIO_THRESHOLD:
+                break
+        else:
+            return None
+        return self.match_name(member["FirstName"], member["LastName"], discord_id)
---
+++ src/scruffy.py
@@ -0,0 +1,206 @@
+import os
+import logging
+import sys
+import yaml
+import discord
+from discord.ext import commands
+import introductions
+
+
+VERSION = 4
+logger = logging.getLogger("Scruffy")
+config = {
+    "ExpectedVersion": VERSION,
+    "DebugMode": True,
+    "LogFile":  "scruffy.log",
+    "ClientSecret": None,
+    "Commands": {
+        "Enabled": True,
+        "Prefix": "scruffy> "
+    },
+    "Introductions": {
+        "Enabled": False,
+        "ChannelId": None,
+        "CommonRoles": [],
+        "GroupsToRolesMapping": {},
+        "JournalReadPath": None,
+        "JournalWritePath": None,
+        "FuzzyMatchMaxLength": 0
+    },
+    "LocalizedMessages": {
+        "Join.Prompt": "Hello, {user.name}!",
+        "Introduction.NickUsedError": "You've already introduced!",
+        "Introduction.NotInJournal": "Sorry, but you're not on the list.",
+    }
+}
+
+
+# Load or generate config file
+if len(sys.argv) > 2:
+    print(f"Usage: python {sys.argv[0]} [ConfigFile]")
+    sys.exit(1)
+config_file = sys.argv[1] if len(sys.argv) == 2 else ""
+if not os.path.isfile(config_file):
+    config_file = config_file or "scruffy-settings.yml"
+    with open(config_file, "w") as default_file:
+        default_file.write(yaml.dump(config))
+    print(f"Default config saved to {config_file}")
+    print(f"Please edit it and start bot with: python {sys.argv[0]} {config_file}")
+    sys.exit(0)
+with open(config_file) as cfg:
+    config.update(yaml.safe_load(cfg))
+if config["ExpectedVersion"] != VERSION:
+    print("Warning: config file can be out of date for this version")
+
+# Set up logs
+log_severity_level = logging.DEBUG if config["DebugMode"] else logging.INFO
+log_formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s]  %(message)s')
+log_file_handler = logging.FileHandler(config["LogFile"])
+log_file_handler.setLevel(log_severity_level)
+log_file_handler.setFormatter(log_formatter)
+log_console_handler = logging.StreamHandler()
+log_console_handler.setLevel(log_severity_level)
+log_console_handler.setFormatter(log_formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(log_console_handler)
+logger.addHandler(log_file_handler)
+discord_logger = logging.getLogger("discord")
+discord_logger.handlers = []
+discord_logger.addHandler(log_console_handler)
+discord_logger.addHandler(log_file_handler)
+
+# Read journal for introductions
+if config["Introductions"]["Enabled"]:
+    intro_journal = introductions.Journal()
+    intro_journal.read(config["Introductions"]["JournalReadPath"])
+
+
+intents = discord.Intents.default()
+intents.members = True
+Scruffy = commands.Bot(command_prefix=config["Commands"]["Prefix"], intents=intents)
+
+
+@Scruffy.command()
+async def hello(ctx):
+    logger.debug("Executing 'hello' command")
+    await ctx.send(f"Hello {ctx.author.name}! I'm **Scruffy_v1.{VERSION}**")
+
+
+@Scruffy.command()
+@commands.has_permissions(administrator=True)
+async def test(ctx):
+    logger.debug("Executing 'test' command")
+    await ctx.send(f"Hello {ctx.author.name} has admin permission")
+
+
+@Scruffy.command()
+@commands.has_permissions(administrator=True)
+async def add_role(ctx, target_role: discord.Role, current_role: discord.Role):
+    if not isinstance(ctx.author, discord.Member):
+        await ctx.send("This can be only issued on a server channel")
+        return
+    logger.info(f"Adding role {target_role} to {current_role}")
+    modified = 0
+    for m in current_role.members:
+        await m.add_roles(target_role)
+        logger.debug(f"Added role {target_role} to user {m}")
+        modified += 1
+    await ctx.send(f"Done! Granted role to {modified} server members.")
+
+
+@Scruffy.command()
+@commands.has_permissions(administrator=True)
+async def remove_role(ctx, target_role: discord.Role, current_role: discord.Role):
+    if not isinstance(ctx.author, discord.Member):
+        await ctx.send("This can be only issued on a server channel")
+        return
+    logger.info(f"Removing role {target_role} from {current_role}")
+    modified = 0
+    for m in current_role.members:
+        await m.remove_roles(target_role)
+        logger.debug(f"Removed role {target_role} from user {m}")
+        modified += 1
+    await ctx.send(f"Done! Removed role from {modified} server members.")
+
+
+@Scruffy.command()
+@commands.has_permissions(administrator=True)
+async def write_introduction_journal(ctx):
+    if file := config["Introductions"]["JournalWritePath"]:
+        intro_journal.save(file)
+        logger.info(f"Saved updated journal to {file}")
+
+
+@Scruffy.event
+async def on_member_join(member: discord.Member):
+    if prompt := config["LocalizedMessages"]["Join.Prompt"]:
+        await member.send(prompt.format(user=member))
+        logger.debug(f"Sent prompt to {member}")
+
+
+@Scruffy.event
+async def on_message(message: discord.Message):
+    if config["Introductions"]["Enabled"] and message.channel.id == config["Introductions"]["Channel"]:
+        await handle_introduction(message)
+    await Scruffy.process_commands(message)
+
+
+async def handle_introduction(message):
+    logger.debug(f"Introduction received")
+    try:
+        first_name, last_name = list(x.strip() for x in message.content.split(",", maxsplit=1))
+    except ValueError:
+        return
+
+    # Extract group and find user in journal
+    group = None
+    try:
+        limit_len = config["Introductions"]["FuzzyMatchMaxLength"]
+        if len(first_name) <= limit_len and len(last_name) <= limit_len:
+            group = intro_journal.match_name_weak(first_name, last_name, message.author.id)
+        else:
+            group = intro_journal.match_name(first_name, last_name, message.author.id)
+    except introductions.NickInUseError:
+        logger.warning(f"Member {message.author} tried to introduce twice")
+        await message.author.send(config["LocalizedMessages"]["Introduction.NickUsedError"])
+        return
+
+    # Select roles to assign
+    roles = set(config["Introductions"]["CommonRoles"])
+    if not group:
+        await message.author.send(config["LocalizedMessages"]["Introduction.NotInJournal"])
+        logger.warning(f"Member {message.author} ({first_name} {last_name}) not found in journal")
+        return
+    if group in config["Introductions"]["GroupsToRolesMapping"]:
+        roles.update(config["Introductions"]["GroupsToRolesMapping"][group])
+    else:
+        logger.warning(f"Group {group} has no roles assigned")
+
+    # Change nickname; this may fail for admins
+    try:
+        await message.author.edit(nick=f"{first_name} {last_name}")
+    except discord.errors.Forbidden:
+        logger.warning(f"Permission error when trying to set nickname for {message.author.name}; is it an admin?")
+
+    # Assign roles
+    assigned = []
+    for role in await message.guild.fetch_roles():
+        if role.name in roles or role.id in roles:
+            await message.author.add_roles(role)
+            logger.debug(f"Assigned role {role.name}{{id={role.id}}} to {message.author}")
+            assigned.append(role.name)
+    if len(assigned) < len(roles):
+        logger.warning(f"Not all roles {roles} for {message.author} found in the server ({len(assigned)}/{len(roles)})")
+    else:
+        logger.info(f"Assigned roles {roles} to member {first_name} {last_name} ({message.author})")
+
+    notification = config["LocalizedMessages"]["Introduction.Success"] \
+        .format(user=message.author, group=group, roles_list=", ".join(assigned),
+                first_name=first_name, last_name=last_name)
+    await message.author.send(notification)
+
+
+# Run the bot
+logger.info("Scruffy reporting in!")
+Scruffy.run(config["ClientSecret"])
+logger.info("Putting Scruffy in the sleep mode, bye!")
